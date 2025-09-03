17cd68d03f0a6f6c39f7b477b0bab2079ccfcc00
kensan-minoh <tanakakenzo2008@gmail.com>
2025-09-03 12:08:39 +0900

    some changes

--- main.py
+++ main.py
@@ -3,15 +3,82 @@ import random
 from pygame.math import Vector2
 
 class Game:
-    def __init__(self):
+    def __init__(self, platform_group):
         self.music = pygame.mixer.music.load('sounds/game_sound.wav')
-        pygame.mixer.music.set_volume(0.2)
+        pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(-1)
+        self.message_font = pygame.font.Font('fonts/Poultrygeist.ttf', size=64)
+        self.font_hud = pygame.font.Font('fonts/Pixel.ttf')
+        # self.tile1_image = pygame.transform.scale(pygame.image.load('images/tiles/Tile (2).png').convert_alpha(),(32, 32))
+        self.tile_images = [pygame.transform.scale(pygame.image.load(f'images/tiles/Tile ({n+1}).png').convert_alpha(),(32, 32)) for n in range(5)]
+        print(self.tile_images)
+        self.platform_group = platform_group
 
-        tile_map = [
-            
+        self.tile_map = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ]
 
+        self.making_map()
+        self.openning_messege()
+
+    def making_map(self):
+        for i in range(23):
+            for j in range(40):
+                if self.tile_map[i][j] == 1:
+                    Tiles(self.tile_images[0], 32 * j, 32 * i, self.platform_group)
+                if self.tile_map[i][j] == 2:
+                    Tiles(self.tile_images[1], 32 * j, 32 * i, self.platform_group)
+
+    def openning_messege(self):
+        self.title = self.message_font.render('Zombie Knight', True, 'green')
+        self.title_rect = self.title.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2))
+        self.message_begin = self.message_font.render("Press 'Enter' to Begin", True, 'white')
+        self.message_begin_rect = self.message_begin.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 80))
+        display_surface.blit(self.title, self.title_rect)
+        display_surface.blit(self.message_begin, self.message_begin_rect)
+        pygame.display.update()
+        self.pause_game()
+
+    def pause_game(self):
+        is_wait = True
+        while is_wait:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        is_wait = False
+
+
+class Tiles(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, groups):
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+        
 
 
 
@@ -20,13 +87,14 @@ class Game:
 pygame.init()
 
 # set display surface
-WINDOW_WIDTH = 1526
-WINDOW_HEIGHT = 1024
+WINDOW_WIDTH = 1280  #1280/32=40
+WINDOW_HEIGHT = 736    #736/32=23
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Zombie Night')
 
 # background
 background_image = pygame.image.load('images/background.png')
+# background_image = pygame.transform.scale(pygame.image.load('images/background.png'), (1280, 736))
 
 
 
@@ -34,7 +102,12 @@ background_image = pygame.image.load('images/background.png')
 clock = pygame.time.Clock()
 FPS =60
 
-my_game = Game()
+
+
+# sprite groups
+platform_group = pygame.sprite.Group()
+
+my_game = Game(platform_group)
 # main game loop
 running = True
 while running:
@@ -46,6 +119,7 @@ while running:
     
     # fill the background
     display_surface.blit(background_image, (0, 0))
+    platform_group.draw(display_surface)
 
 
     # update the display
