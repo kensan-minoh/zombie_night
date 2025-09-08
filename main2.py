be6b27fd2762e3cbd7198cd1a762153c5cc71f3f
kensan-minoh <tanakakenzo2008@gmail.com>
2025-09-08 13:23:38 +0900

    some more changes

--- main2.py
+++ main2.py
@@ -3,7 +3,7 @@ import random
 from pygame.math import Vector2
 
 class Game:
-    def __init__(self, player_group, platform_group):
+    def __init__(self, ruby_group, player_group, platform_group):
         self.music = pygame.mixer.music.load('sounds/game_sound.wav')
         pygame.mixer.music.set_volume(0.05)
         pygame.mixer.music.play(-1)
@@ -11,14 +11,15 @@ class Game:
         self.font_hud = pygame.font.Font('fonts/Pixel.ttf')
         # self.tile1_image = pygame.transform.scale(pygame.image.load('images/tiles/Tile (2).png').convert_alpha(),(32, 32))
         self.tile_images = [pygame.transform.scale(pygame.image.load(f'images/tiles/Tile ({n+1}).png').convert_alpha(),(32, 32)) for n in range(5)]
-        print(self.tile_images)
+
         self.platform_group = platform_group
         self.player_group = player_group
+        self.ruby_group = ruby_group
         # tile map size is 23 rows and 40 columns
         self.tile_map = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'P', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,'P', 0, 0, 0, 0, 0, 'R', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [4, 4, 4, 4, 4, 4, 4 ,4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 4, 4, 4, 4, 4, 4, 4 ,4, 4, 4, 4, 4],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,6 +60,8 @@ class Game:
                     Tiles(self.tile_images[4], 32 * j, 32 * i, self.platform_group)
                 if self.tile_map[i][j] == 'P':
                     self.player = Player(32 * j, 32 * i - 32, self.platform_group, self.player_group)
+                if self.tile_map[i][j] == 'R':
+                    self.mother_ruby = Rubies(32 * j, 32 * i - 32, self.ruby_group)
 
     def openning_messege(self):
         self.title = self.message_font.render('Zombie Knight', True, 'green')
@@ -90,14 +93,18 @@ class Tiles(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
         def __init__(self, x, y, platform_group, groups):
             super().__init__(groups)
+            self.player_size = 64
 
-            self.player_idle_animation = [pygame.transform.scale(pygame.image.load(f'images/player/idle/Idle ({n+1}).png').convert_alpha(), (64, 64)) for n in range(10)]
-            self.player_attack_animation = [pygame.transform.scale(pygame.image.load(f'images/player/attack/Attack ({n+1}).png').convert_alpha(), (64, 64)) for n in range(10)]
-            self.player_jump_animation = [pygame.transform.scale(pygame.image.load(f'images/player/jump/Jump ({n+1}).png').convert_alpha(), (64, 64)) for n in range(10)]
-            self.player_run_animation_right = [pygame.transform.scale(pygame.image.load(f'images/player/run/Run ({n+1}).png').convert_alpha(), (64, 64)) for n in range(10)]
+            self.player_idle_animation_right = [pygame.transform.scale(pygame.image.load(f'images/player/idle/Idle ({n+1}).png').convert_alpha(), (self.player_size, self.player_size)) for n in range(10)]
+            self.player_idle_animation_left = [pygame.transform.flip(image, True, False) for image in self.player_idle_animation_right]
+            self.player_attack_animation_right = [pygame.transform.scale(pygame.image.load(f'images/player/attack/Attack ({n+1}).png').convert_alpha(), (self.player_size, self.player_size)) for n in range(10)]
+            self.player_attack_animation_left = [pygame.transform.flip(image, True, False) for image in self.player_attack_animation_right]
+            self.player_jump_animation_right = [pygame.transform.scale(pygame.image.load(f'images/player/jump/Jump ({n+1}).png').convert_alpha(), (self.player_size, self.player_size)) for n in range(10)]
+            self.player_jump_animation_left = [pygame.transform.flip(image, True, False) for image in self.player_jump_animation_right]
+            self.player_run_animation_right = [pygame.transform.scale(pygame.image.load(f'images/player/run/Run ({n+1}).png').convert_alpha(), (self.player_size, self.player_size)) for n in range(10)]
             self.player_run_animation_left = [pygame.transform.flip(image, True, False) for image in self.player_run_animation_right]
 
-            self.image = self.player_idle_animation[0]
+            self.image = self.player_idle_animation_right[0]
             self.rect = self.image.get_rect(topleft=(x, y))
             self.mask = pygame.mask.from_surface(self.image)
 
@@ -116,12 +123,14 @@ class Player(pygame.sprite.Sprite):
             self.acceleration = Vector2(0, self.VERTICAL_ACCELERATION)
 
             self.is_grounded = False
+            self.facing = 'right'
             self.platform_group = platform_group
 
         def update(self):
             self.move()
             self.animation()
             self.check_status()
+            self.choose_animation()
 
         def animation(self):
             self.animation_number += 0.2
@@ -150,15 +159,30 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.is_grounded = False
         
+        def choose_animation(self):
+
+            if self.is_grounded:
+                if self.facing == 'right':
+                    if self.velocity.x > 1:
+                        self.animation_sprite = self.player_run_animation_right
+                    else:
+                        self.animation_sprite = self.player_idle_animation_right
+                else:
+                    if self.velocity.x < -1:
+                        self.animation_sprite = self.player_run_animation_left
+                    else:
+                        self.animation_sprite = self.player_idle_animation_left                     
+        
 
 
         def move(self):
             keys = pygame.key.get_pressed()
             if keys[pygame.K_RIGHT]:
                 self.acceleration.x = self.HORIZONTAL_ACCELERATION
+                self.facing = 'right'
             elif keys[pygame.K_LEFT]:
-
                 self.acceleration.x = - self.HORIZONTAL_ACCELERATION
+                self.facing = 'left'
             else:
                 self.acceleration.x = 0
 
@@ -167,8 +191,41 @@ class Player(pygame.sprite.Sprite):
             self.velocity += self.acceleration
             self.position += self.velocity + 0.5 * self.acceleration
             
+            self.wrap_around_motion()
+            
             # change the rect
             self.rect.topleft = (int(self.position.x), int(self.position.y))
+
+        def wrap_around_motion(self):
+            if self.position.x < 0:
+                self.position.x = WINDOW_WIDTH - self.player_size
+            elif self.position.x + self.player_size > WINDOW_WIDTH:
+                self.position.x = 0
+
+class Rubies(pygame.sprite.Sprite):
+    def __init__(self, x, y, *groups):
+        super().__init__(*groups)
+        self.ruby_size = 64
+        self.ruby_animation = [pygame.transform.scale(pygame.image.load(f'images/ruby/sprite_ruby{n}.png').convert_alpha(), (self.ruby_size, self.ruby_size)) for n in range(7)]
+        self.animation_sprite = self.ruby_animation
+        self.animation_number = 0
+        self.image = self.ruby_animation[self.animation_number]
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+
+    def update(self):
+
+        self.animation()
+
+    def animation(self):
+        # print(self.animation_number)
+        self.animation_number += 0.15
+        if self.animation_number >= len(self.animation_sprite):
+            self.animation_number = 0
+        else:
+            self.image = self.animation_sprite[int(self.animation_number)]
+
+
 
 # initialize pygame
 pygame.init()
@@ -189,8 +246,9 @@ FPS =60
 # sprite groups
 platform_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+ruby_group = pygame.sprite.Group()
 
-my_game = Game(player_group, platform_group)
+my_game = Game(ruby_group, player_group, platform_group)
 my_player = my_game.player
 
 
@@ -212,6 +270,8 @@ while running:
     platform_group.draw(display_surface)
     player_group.update()
     player_group.draw(display_surface)
+    ruby_group.update()
+    ruby_group.draw(display_surface)
 
     # update the display
     pygame.display.update()
